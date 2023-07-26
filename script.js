f94073d9b407c54b213344e4704a5bf720136a45
StainedS <105196885+StainedS@users.noreply.github.com>
2023-07-26 12:54:38 +0530

    first

---
+++ script.js
@@ -0,0 +1,61 @@
+const accesskey = "hDKCtAwCSmMfKQNPYlCtZiz0Y8572-INU-LIYyh7x24";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-button");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+
+let inputData = "";
+let page = 1 ;
+
+async function searchImages(){
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesskey}`
+    
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(response)
+    console.log(data)
+
+    const results = data.results;
+    console.log(results)
+
+    if(page === 1){
+            searchResults.innerHTML = "";
+    }
+
+    results.map((result) => {
+        const imageWrapper = document.createElement('div')
+        imageWrapper.classList.add("search-result")
+        const image = document.createElement('img')
+        image.src = result.urls.small
+        image.alt = result.alt_description
+        const imageLink = document.createElement('a')
+        imageLink.href = result.links.html
+        imageLink.target = "_blank"
+        imageLink.textContent = result.alt_description
+
+        imageWrapper.appendChild(image)
+        imageWrapper.appendChild(imageLink)
+        searchResults.appendChild(imageWrapper)
+    });
+
+    page++
+    if(page > 1 ){
+        showMore.style.display= "block";
+    }
+}
+
+
+formEl.addEventListener("submit",(event) =>{
+    event.preventDefault();
+    page = 1;
+    searchImages()
+})
+
+
+showMore.addEventListener("click",() =>{
+    searchImages()
+})
